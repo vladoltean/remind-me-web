8fb9573641c4ce20a2bdbbf2354c82d0e58e18d0
vlad.oltean <vladoltean1193@gmail.com>
2018-07-13 14:30:09 +0300

    Add react bootstrap. Login layout.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Remind Me</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="vertical-align">
+                <div className="container">
+                    <div className="row">
+                        {/*<div name="sign-on-container" className="col-sm-2 col-sm-offset-5 col-md-2 col-md-offset-5 col-lg-2 col-lg-offset-5">*/}
+                        <div name="sign-on-container" className="horizontal-align sign-on">
+                            <a className="btn-primary">Facebook Login</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
