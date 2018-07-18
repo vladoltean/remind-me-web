426c0254e0e8092ed021ed84a80e4fb2a4b05267
vlad.oltean <vladoltean1193@gmail.com>
2018-07-18 12:14:08 +0300

    Login page visuals.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,47 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="vertical-align">
+            <div className="vertical-align page">
                 <div className="container">
                     <div className="row">
-                        {/*<div name="sign-on-container" className="col-sm-2 col-sm-offset-5 col-md-2 col-md-offset-5 col-lg-2 col-lg-offset-5">*/}
                         <div name="sign-on-container" className="horizontal-align sign-on">
-                            <a className="btn-primary">Facebook Login</a>
+                            <div className="row">
+                                <div className="row">
+                                    <div className="sign-in-title">
+                                        Sign in
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="sign-in-content">
+
+                                        <div className="row">
+                                            <div className="sign-in">
+                                                <form>
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" placeholder="Enter Username" name="uname"
+                                                               required/>
+                                                    </div>
+
+                                                    <div className="form-group">
+                                                        <input type="password" className="form-control" placeholder="Enter Password" name="psw"
+                                                               required/>
+                                                    </div>
+
+                                                    <button className="btn btn-default my-button">Login</button>
+                                                </form>
+                                            </div>
+                                        </div>
+
+                                        <div className="row social-sign-in">
+                                            <div className="row title">Social Sign In</div>
+                                            <div className="row">
+                                                <button className="btn my-button fb-sign-in">Facebook</button>
+                                                <button className="btn my-button google-sign-in">Google</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
